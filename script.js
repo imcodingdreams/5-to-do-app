548633b20478248b13ebd6043cd799df597a31a6
imcodingdreams <naykavallejo@yahoo.com>
2022-09-14 14:28:55 -0700

    Create addItems function and modify CSS

--- script.js
+++ script.js
@@ -1,9 +1,54 @@
-button.addEventListener("click", addItems)
-function addItems() {
-  let newItem = document.getElementById("item-added").value;
-  document.getElementById("item-added").value = ''; 
-  return document.getElementById("new-item").innerText = newItem; 
-}
+//selectors
+const itemInput = document.getElementById('itemInput');
+const addItemButton = document.getElementById('addItemButton');
+const itemsList = document.getElementById('itemsList');
+//const deleteBtn = document.getElementById('deleteBtn')
 
+//eventListeners
+addItemButton.addEventListener('click', addNewItem);
+//deleteBtn.addEventListener('click', deleteItem);
+//checkbox.addEventListener('click', crossItem);
 
-let itemsList = [];
+//functons
+function addNewItem(event) {
+  event.preventDefault();
+
+  //Create div
+  const itemsListDiv = document.createElement('div');
+  itemsListDiv.setAttribute('id', 'item');
+
+  //Create checkbox
+  const checkbox = document.createElement('button');
+  checkbox.innerHTML = '<i class="fa-sharp fa-solid fa-check"></i>';
+  checkbox.setAttribute('id', 'checkbox');
+  itemsListDiv.appendChild(checkbox);
+  //Create li
+  const newItem = document.createElement('li');
+  newItem.innerText = itemInput.value;
+  newItem.setAttribute('id', 'newItem');
+  itemsListDiv.appendChild(newItem);
+  //Create delete button
+  const deleteBtn = document.createElement('button');
+  deleteBtn.innerHTML = '<i class="fa-regular fa-trash-can"></i>';
+  deleteBtn.setAttribute('id', 'deleteBtn');
+  itemsListDiv.appendChild(deleteBtn);
+
+  itemInput.value = '';
+
+  itemsList.appendChild(itemsListDiv);
+};
+
+function deleteItem () {
+  const elem = document.querySelector('#hint');
+  elem.remove('ul');
+};
+
+function crossItem () {
+  let doneTodo = itemInput.value;
+  let result = doneTodo.onclick.strike(checkbox);
+  document.getElementById(checkbox).innerHTML = doneTodo;
+  return result
+};
+
+//Delete item once deleteBtn is clicked
+//Cross item's text once checkbox is clicked
